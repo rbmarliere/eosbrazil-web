d2d04645ce655ae75746cbf74c09cb66bd0c7e38
Calexxxxx <pasm28@hotmail.nl>
2018-05-27 10:05:25 +0200

    Refactor to bootstrap

--- src/Components/Masthead.js
+++ src/Components/Masthead.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
 import Logo from '../Assets/EOS-Brazil_LogoMark_2_Color_250_text_shadow.png';
+
 import Logo_300 from '../Assets/EOS-Brazil_LogoMark_2_Color_300_text_shadow.png';
+
 import Logo_500 from '../Assets/EOS-Brazil_LogoMark_2_Color_500_text_shadow.png';
+
 class Masthead extends Component {
     render() {
         return (
-            <section className='masthead'>
+            <section className='jumbotron'>
                 <picture>
                     <source media="(min-width: 1440px)" srcSet={Logo_500} />
                     <source media="(min-width: 768px)" srcSet={Logo_300} />
