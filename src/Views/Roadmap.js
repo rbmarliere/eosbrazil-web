bf7c6a69f32ee678feea52dd6e9f56113205c117
Calexxxxx <pasm28@hotmail.nl>
2018-05-28 10:24:02 +0200

    Initial commit

---
+++ src/Views/Roadmap.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Roadmap extends Component {
+    render() {
+        return (
+            <h1>working</h1>
+        )
+    }
+}
+
+export default Roadmap;
