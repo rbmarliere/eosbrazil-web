8e4e3c2485351a9e8e661e68007e10952e9a97fd
Calexxxxx <pasm28@hotmail.nl>
2018-05-28 10:22:01 +0200

    change to bootstrap classes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import logo_250 from '../Assets/logo_250.png';
 class Footer extends Component {
     render() {
         return (
-            <footer className='footer'>
+            <footer className='footer d-flex justify-content-center mb-2'>
                 <picture>
                     <source media="(min-width: 768px)" srcSet={logo_250} />
                     <img src={logo} alt="" className='footer__logo'/>
