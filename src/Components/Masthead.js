bf637f97483608fecca057316478988729c6db24
Calexxxxx <pasm28@hotmail.nl>
2018-05-29 14:40:59 +0200

    Add extra source for 1024px screens

--- src/Components/Masthead.js
+++ src/Components/Masthead.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Logo from '../Assets/EOS-Brazil_LogoMark_2_Color_250_text_shadow.png';
 
 import Logo_300 from '../Assets/EOS-Brazil_LogoMark_2_Color_300_text_shadow.png';
-
+import Logo_400 from '../Assets/EOS-Brazil_LogoMark_2_Color_400_text_shadow.png';
 import Logo_500 from '../Assets/EOS-Brazil_LogoMark_2_Color_500_text_shadow.png';
 
 class Masthead extends Component {
@@ -12,6 +12,7 @@ class Masthead extends Component {
             <section className='jumbotron'>
                 <picture>
                     <source media="(min-width: 1440px)" srcSet={Logo_500} />
+                    <source media="(min-width: 1024px)" srcSet={Logo_400} />
                     <source media="(min-width: 768px)" srcSet={Logo_300} />
                     <img src={Logo} alt="" className='logo logo-big'/>
                 </picture>
