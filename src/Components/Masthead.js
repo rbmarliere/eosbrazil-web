e98bef9b2ff7755ccf0c05390d01821ffe71f389
Calexxxxx <pasm28@hotmail.nl>
2018-05-29 22:43:16 +0200

    Add logo masthead

--- src/Components/Masthead.js
+++ src/Components/Masthead.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 
-// import Logo from '../Assets/EOS-Brazil_LogoMark_2_Color_250_text_shadow.webp';
-//
-// import Logo_300 from '../Assets/EOS-Brazil_LogoMark_2_Color_300_text_shadow.webp';
-// import Logo_400 from '../Assets/EOS-Brazil_LogoMark_2_Color_400_text_shadow.webp';
-// import Logo_500 from '../Assets/EOS-Brazil_LogoMark_2_Color_500_text_shadow.webp';
+import Logo from '../Assets/logo-masthead-150.webp';
+import Logo_300 from '../Assets/logo-masthead-300.webp';
+import Logo_400 from '../Assets/logo-masthead-400.webp';
 
 class Masthead extends Component {
     render() {
         return (
-            <section className='jumbotron'>
+            <section className='jumbotron text-center'>
                 <picture>
-                    {/*<source media="(min-width: 1440px)" srcSet={Logo_500} />*/}
-                    {/*<source media="(min-width: 1024px)" srcSet={Logo_400} />*/}
-                    {/*<source media="(min-width: 768px)" srcSet={Logo_300} />*/}
-                    {/*<img src={Logo} alt="" className='logo logo-big'/>*/}
+                    <source media="(min-width: 1024px)" srcSet={Logo_400} />
+                    <source media="(min-width: 768px)" srcSet={Logo_300} />
+                    <img src={Logo} alt="" className='logo logo-big'/>
                 </picture>
+                <div className="row justify-content-center">
+                    <div className="col-12 text-center">
+                        <h2 className='logo-text eos text-center'>EOS</h2>
+                        <h2 className='logo-text brazil text-center'>BRAZIL</h2>
+                    </div>
+                </div>
             </section>
         )
     }
