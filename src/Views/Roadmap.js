a516036399f7d690817b0593b4be18a9265fcefb
Calexxxxx <pasm28@hotmail.nl>
2018-05-29 23:38:49 +0200

    Update Roadmap new text

--- src/Views/Roadmap.js
+++ src/Views/Roadmap.js
@@ -14,7 +14,7 @@ class Roadmap extends Component {
                     </div>
 
                     <div className="row mb-5">
-                        <div className="col-xs-12 col-md-10 offset-md-1 justify-content-center">
+                        <div className="col-xs-10 col-md-8 offset-md-2 justify-content-center">
                             <div className="timeline text-justify">
                                 <div className="card card-timeline left">
                                     <div className="card-header">
@@ -84,6 +84,31 @@ class Roadmap extends Component {
                                         <h5 className="card-title">Server Expansion</h5>
                                         <p className="card-text">Begin moving off AWS cloud infrastructure towards our own physical servers.</p>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row mb-5 mt-5">
+                        <div className="col-xs-10 col-md-8 offset-md-2 justify-content-center">
+                            <h2 className="text-center">Commitments</h2>
+                        </div>
+                    </div>
+
+                    <div className="row mb-5">
+                        <div className="col-xs-10 col-md-8 offset-md-2 justify-content-center">
+                            <div className="card mb-2">
+                                <div className="card-body">
+                                    <h5 className="card-title mt-4 mb-4">Blog updates</h5>
+                                    <p className="card-text mt-4 mb-4">We would like to keep the community regularly updated on various developments in the EOS and block producer scene. This will be written in English and Portuguese.</p>
+                                    <h5 className="card-subtitle mt-4 mb-4">Community dapp contributions and our stance on voter kickbacks</h5>
+                                    <p className="card-text mt-4 mb-4">
+                                        Great discussions were set since the start of the governance channel about kickbacks. Following the majority of community sentiment, our stance is that rewarding voters directly does not provide value to the community. For every token we distribute to voters it is one less that could be used to contribute to more meaningful endeavours. So we will not be paying dividends to voters. Instead we will pledge to leverage community projects, with an emphasis on projects originating in South America, using our block producing rewards after covering the costs of our operation. This way the demand for EOS increases organically and the value of your tokens increase without creating a race among candidates to cut profit margins.
+                                    </p>
+                                    <h5 className="card-subtitle mt-4 mb-4">Earnings transparency report</h5>
+                                    <p className="card-text mt-4 mb-2">
+                                        We will provide a quarterly report of our expenses which show exactly how our money is being used.
+                                    </p>
                                 </div>
                             </div>
                         </div>
