952f648a61c59062ff874a9304f154d5bd7cde3e
Calexxxxx <pasm28@hotmail.nl>
2018-05-29 14:43:15 +0200

    Fix layout specs page

--- src/Views/Specs.js
+++ src/Views/Specs.js
@@ -7,9 +7,9 @@ class Specs extends Component {
         return (
             <Fragment>
                 <div className="container">
-                    <div className="row mt-10 justify-content-center">
+                    <div className="row mt-10 justify-content-center mb-5">
                         <div className="col-xs-10 col-md-8">
-                            <div className="card mb-5">
+                            <div className="card mb-2">
                                 <div className="card-body">
                                     <h5 className="card-title mt-2 mb-2">Main producer node and full nodes</h5>
                                     <ul className="list-group list-group-flush">
@@ -22,25 +22,25 @@ class Specs extends Component {
                                 </div>
                             </div>
 
-                            <div className="card mb-5">
+                            <div className="card mb-2">
                                 <div className="card-body">
-                                    <h5 className="card-title mt-2 mb-2">Proxy servers</h5>
+                                    <h5 className="card-title mt-4 mb-4">Proxy servers</h5>
                                     <ul className="list-group list-group-flush">
                                         <li className="list-group-item text-black-50">t2.large burstable performance instances</li>
                                     </ul>
-                                    <p className="card-text mt-2 mb-2">We have already made a reasonable investment in a few bare metal servers in order to move off of AWS into a data center when possible and have more direct control over scaling</p>
-                                    <h5 className="card-subtitle mt-2 mb-2">Ubuntu Linux</h5>
+                                    <p className="card-text mt-4 mb-4">We have already made a reasonable investment in a few bare metal servers in order to move off of AWS into a data center when possible and have more direct control over scaling</p>
+                                    <h5 className="card-subtitle mt-4 mb-4">Ubuntu Linux</h5>
                                     <ul className="list-group list-group-flush">
                                         <li className="list-group-item text-black-50">Lenovo Thinkpad P71 x2</li>
                                         <li className="list-group-item text-black-50">i7 7820HQ</li>
                                         <li className="list-group-item text-black-50">64GB RAM and 32GB RAM</li>
                                     </ul>
-                                    <h5 className="card-subtitle mt-2 mb-2">iMac Pro</h5>
+                                    <h5 className="card-subtitle mt-4 mb-4">iMac Pro</h5>
                                     <ul className="list-group list-group-flush">
                                         <li className="list-group-item text-black-50">8 core Xeon W</li>
                                         <li className="list-group-item text-black-50">32GB RAM</li>
                                     </ul>
-                                    <p className="card-text mt-2 mb-2">
+                                    <p className="card-text mt-4 mb-4">
                                         There are currently some issues on the Mac build of EOS software which preclude us from using
                                         MacOS, but we’d like to experiment with it in the future if possible and see if we can make
                                         it viable as a failover node.
@@ -48,9 +48,9 @@ class Specs extends Component {
                                 </div>
                             </div>
 
-                            <div className="card mb-5">
+                            <div className="card mb-2">
                                 <div className="card-body">
-                                    <h5 className="card-title mt-2 mb-2">Storage</h5>
+                                    <h5 className="card-title mb-4">Storage</h5>
                                     <p className="card-text">We are still very hesitant about the idea of block producers providing IPFS storage, but we
                                         are tentatively going to have a single d2 instance (somewhere from 12-48 TB in size) for the
                                         purpose of providing rudimentary storage. Anything further will require us to move to bare
@@ -62,7 +62,7 @@ class Specs extends Component {
 
                             <div className="card mb-5">
                                 <div className="card-body">
-                                    <h5 className="card-title mt-2 mb-2">Security</h5>
+                                    <h5 className="card-title mb-4">Security</h5>
                                     <p className="card-text">We have rudimentary DDoS protection in place using port knocking, haproxy (which doubles as a
                                         load balancer), fail2ban, and Wireguard VPN. More thorough and expensive protection will be
                                         contingent on our election and ability to scale, at which point we will investigate
