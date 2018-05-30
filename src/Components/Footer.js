6e1e84329820560813209b908ced33f8cdde2760
Ricardo Marliere <rbmarliere@gmail.com>
2018-05-29 23:16:42 -0300

    small fixes on Components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
             <footer className='container-fluid'>
                 <div className="row d-flex justify-content-center pt-2 pb-2">
                     <div className="col-xs-12">
-                        <p className="text-justify">EOS Brazil {(new Date().getFullYear())} &copy; contact@eosbrazil.com</p>
+                        <p className="lightgray text-justify">EOS Brazil {(new Date().getFullYear())} &copy; contact@eosbrazil.com</p>
                     </div>
                 </div>
 
