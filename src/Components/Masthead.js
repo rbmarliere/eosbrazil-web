0fa0ce44293956cb616224292597c8627f7f6244
Calexxxxx <pasm28@hotmail.nl>
2018-05-30 10:13:34 +0200

    Make logo smaller on big screens

--- src/Components/Masthead.js
+++ src/Components/Masthead.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 
 import Logo from '../Assets/logo-masthead-150.png';
 import Logo_300 from '../Assets/logo-masthead-300.png';
-import Logo_400 from '../Assets/logo-masthead-400.png';
 
 class Masthead extends Component {
     render() {
         return (
             <section className='jumbotron text-center'>
                 <picture>
-                    <source media="(min-width: 1024px)" srcSet={Logo_400} />
                     <source media="(min-width: 768px)" srcSet={Logo_300} />
                     <img src={Logo} alt="" className='logo logo-big'/>
                 </picture>
